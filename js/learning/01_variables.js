580de1d832e9263bfff0644d55758e0162e54eef
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-09-29 19:56:24 +0500

    variables done

---
+++ js/learning/01_variables.js
@@ -0,0 +1,12 @@
+console.log("Hello from the Variable page");
+/**
+ * VARIABLE
+ * variable are the buckets for storing data in the RAM
+ * use let or const keyword for declaring variable
+ */
+
+
+const nombre = "Shahzad Farooq";
+console.log(nombre);
+nombre = "Ali Imran";
+console.log(nombre);
--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ let nombre = "Shahzad";
 let age = 34;
 
 console.log(age)
+
+
+/**
+ * variables
+ * operators
+ * if else
+ * arrays
+ * loops
+ * functions
+ * OOP (classes and objects)
+ * 
+ */
+
+
