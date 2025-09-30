bd33309ffe4df50a4abb955f6ccc7f11d6ab6811
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-09-30 20:22:01 +0500

    datatypes done

--- js/learning/01_variables.js
+++ js/learning/01_variables.js
@@ -3,10 +3,39 @@ console.log("Hello from the Variable page");
  * VARIABLE
  * variable are the buckets for storing data in the RAM
  * use let or const keyword for declaring variable
+ * 
+ * DATATYPE
+ * type of data stored in the variable
  */
 
+// asad bhai isko falany model se serial kra lena 
+let name = "Shahzad";
+let age = 34;
+let isMarried = false;
+let whenIWillBeMillionare = undefined;
+let freeTime = null;
+console.log(name,age);
 
-const nombre = "Shahzad Farooq";
-console.log(nombre);
-nombre = "Ali Imran";
-console.log(nombre);
+let ceo = "Shahzad";
+// console.log(ceo)
+ceo = "Zaid Munawar";
+// console.log(ceo)
+ceo = "Muhtshim Daud";
+// console.log(ceo)
+
+
+
+let friend1 = "Zaid Munawar";
+let friend2 = "Muhtshim Daud";
+let friend3 = "Ali Imran";
+
+let friends = ["zaid", "muhtshim", "ali", "sinwar"];
+
+let myInfo = {
+    name : "Shahzad",
+    age : 22,
+    eduction : "BSSE",
+    isMarried : false,
+}
+console.log(myInfo.age);
+
