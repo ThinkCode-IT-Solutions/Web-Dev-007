a72928bfc109ded7c1f91f31be3bbec96a775095
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-10-06 19:30:23 +0500

    switch case done

---
+++ js/learning/04_switch.js
@@ -0,0 +1,63 @@
+console.log("Hello from the switch page");
+
+
+let age = 10;
+
+
+switch (age) {
+    case 10:
+        console.log("you are 10 years old");
+        break;
+    case 12:
+        console.log("you are 12 , you are getting ready for high school");
+        break;
+    case 16:
+        console.log("you are 16 , getting ready for college");
+        break;
+    default:
+        console.log("you are not 10,12,or 16");
+        break;
+}
+
+let nombre = "Noman";
+switch (nombre) {
+    case "noman":
+        console.log("hello noman");
+        break;
+    case "faizan":
+        console.log("Hi Faizan");
+        break;
+    case "usama":
+        console.log("no VISA for USA");
+        break;
+    default:
+        console.log("you are not one of employees");
+        break;
+}
+
+
+let index = 1;
+
+switch (index){
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+}
