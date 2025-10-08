bfbfee85b37d5820e01e1df825103292b050af1e
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-10-08 19:23:44 +0500

    for loop done

---
+++ js/learning/05_loops.js
@@ -0,0 +1,22 @@
+console.log("Loops in JS");
+
+/**
+ * LOOPS
+ * loops run the code multiple times
+ * loops are of three types
+ * - for loop
+ * - while loop
+ * - do while loop
+ * loop need 3 things to run
+ * - initialization
+ * - condition 
+ * - increment/decrement
+ */
+
+
+
+
+for (let index = 0; index < 10; index++) {
+    console.log(index + " => Hello Sir, How are you");
+}
+
