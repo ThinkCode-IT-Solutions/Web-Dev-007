8d0759fa2940776c62f256636e416d0500340230
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-10-09 19:34:31 +0500

    all loops done

--- js/learning/05_loops.js
+++ js/learning/05_loops.js
@@ -11,12 +11,41 @@ console.log("Loops in JS");
  * - initialization
  * - condition 
  * - increment/decrement
+ * loops are mostly used to iterate arrays
  */
 
 
+let friends = [
+    "Zaid", "Muhtshim", "Ali Imran", "Hamza", "Usman", "Bilal", "Ahmad", "Suleman",
+    "Owais", "Taha", "Saad", "Zain", "Hassan", "Huzaifa", "Shahzaib", "Asad",
+    "Fahad", "Noman", "Rehan", "Talha", "Adeel", "Imran", "Rizwan", "Adnan",
+    "Mubashir", "Hammad", "Kamran", "Naveed", "Shahid", "Danish", "Farhan",
+    "Sameer", "Abdul Rafay", "Umer", "Ammar"
+];
 
 
-for (let index = 0; index < 10; index++) {
-    console.log(index + " => Hello Sir, How are you");
-}
 
+
+// for (let index = 0; index < friends.length; index++) {
+//     let friend = friends[index];
+//     console.log(index + " => Hello " + friend + " , How are you");
+// }
+
+
+
+// let index = 0;
+// while (index < friends.length ){
+//     let friend = friends[index];
+//     console.log("Hi " +friend + " have a nice day");
+
+//     index++;
+// }
+
+let index = 0;
+do {
+
+    let friend = friends[index];
+    console.log("Hi " + friend + " have a nice day");
+
+    index++;
+} while (index > friends.length)
