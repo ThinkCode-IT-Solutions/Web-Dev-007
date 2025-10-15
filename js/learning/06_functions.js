f714234f5ec400ec752e538f1d71a7278547b256
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-10-15 19:05:03 +0500

    functions part 2 done

--- js/learning/06_functions.js
+++ js/learning/06_functions.js
@@ -28,15 +28,19 @@ function checkDrivingStatus(name, age, license) {
 
     if (name == "usama") {
         console.log("some error occured , please never try again");
+        return false;
     } else {
         if (age >= 18 && age <= 60) {
             if (license) {
                 alert(name + " You have a license , you can drive legally ");
+                return true;
             } else {
                 alert(name + " you dont have a license but your age qualifies , please register your licese");
+                return false;
             }
         } else {
             alert(name + " you are under or over legal age");
+            return false;
         }
     }
 
@@ -52,7 +56,26 @@ function printTable(table) {
     }
 }
 
+function getPercentage (obtained,total){
+    let percentage = (obtained/total)*100;
+    return percentage;
+}
 
+function getGrades (perc){
+    if(perc >= 90){
+        return "A";
+    }else if (perc >= 80 && perc < 90){
+        return "B";
+    }else if (perc >= 70 && perc < 80){
+        return "C";
+    }else if (perc >= 40 && perc < 70){
+        return "D";
+    }else if (perc < 40){
+        return "F";
+    }else{
+        return null;
+    }
+}
 // 2 X 1 = 2
 let num1 = 34;
 let num2 = 355;
@@ -73,6 +96,24 @@ greetMyFriend("Abdullah")
 printTable(2);
 printTable(98);
 
-checkDrivingStatus("Shahzad", 12, true);
+// let result = checkDrivingStatus("Shahzad", 220, false);
+// console.log("result of driving test is " + result);
 
 
+// alert("hello")
+
+
+
+// take input from user
+let name = prompt("Enter your name");
+let obtainedMarks = parseInt (prompt("Enter your obtained marks"));
+let totalMarks = parseInt( prompt("Enter your total marks"));
+
+// generate percentage
+let  percentage = getPercentage(1001,1100);
+
+// generate grades
+let grade = getGrades(percentage);
+
+
+console.log("Grade: " + grade ,"Percentage: " + percentage);
